65fdf2880636a6e6a06321f1f00d17d0a0d47bbb
luis.talero <luis.talero@wiedii.co>
2025-07-21 09:12:38 -0500

    feat: :rocket: se crea la dockerizacion del proyecto

--- src/src/Views/home.html.php
+++ src/src/Views/home.html.php
@@ -8,7 +8,7 @@
 <body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md text-center">
         <h1 class="text-4xl font-bold text-gray-800 mb-4">¡Bienvenido a tu Gestor de Tareas!</h1>
-        <p class="text-gray-600 mb-6">Esta es la página de inicio de tu proyecto.</p>
+        <p class="text-gray-600 mb-6">¡Bienvenido, estas listo para ordenar todas tus tareas por hacer!.</p>
         <a href="/tasks" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
             Ver mis tareas
         </a>
